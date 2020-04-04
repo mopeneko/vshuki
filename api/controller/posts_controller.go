e657cd0bbc74b1c0b632d0f42f8ce14c510fae32
mopeneko <26901342+mopeneko@users.noreply.github.com>
2020-04-05 03:59:19 +0900

    Implement getting posts

---
+++ api/controller/posts_controller.go
@@ -0,0 +1,29 @@
+package controller
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/mopeneko/vshuki/api/model"
+	"github.com/mopeneko/vshuki/api/view"
+	"net/http"
+)
+
+type PostsController struct {
+	*BaseController
+}
+
+func NewPostsController(base *BaseController) *PostsController {
+	return &PostsController{base}
+}
+
+func (c *PostsController) GetPosts(ctx echo.Context) error {
+	result := &view.GetPostsResult{}
+
+	posts, err := model.PostsModel{}.GetLatestPosts(c.db)
+
+	if err != nil {
+		return view.RenderGetPostsResult(ctx, http.StatusInternalServerError, result)
+	}
+
+	result.Posts = posts
+	return view.RenderGetPostsResult(ctx, http.StatusOK, result)
+}
