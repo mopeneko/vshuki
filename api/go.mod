1a293af2373c6410d1adc9df10b3217b0339d97e
mopeneko <26901342+mopeneko@users.noreply.github.com>
2020-04-05 01:56:49 +0900

    Connect to database from api

--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,8 @@ module github.com/mopeneko/vshuki/api
 go 1.13
 
 require (
+	github.com/jinzhu/gorm v1.9.12
+	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo/v4 v4.1.16
 	golang.org/x/crypto v0.0.0-20200403201458-baeed622b8d8 // indirect
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
--- api/main.go
+++ api/main.go
@@ -1,9 +1,30 @@
 package main
 
-import "github.com/mopeneko/vshuki/api/router"
+import (
+	"github.com/joho/godotenv"
+	"github.com/mopeneko/vshuki/api/database"
+	"github.com/mopeneko/vshuki/api/router"
+	"log"
+)
 
 func main() {
-	e := router.Init()
+	err := loadDotEnv()
+
+	if err != nil {
+		log.Fatalf("Failed to load .env file: %+v\n", err)
+	}
+
+	db, err := database.Init()
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %+v\n", err)
+	}
+
+	e := router.Init(db)
 
 	e.Logger.Fatal(e.Start(":4000"))
+}
+
+func loadDotEnv() error {
+	return godotenv.Load()
 }
