7731b2c0922082e65584f5c66a86e1bbc6c141b4
mopeneko <26901342+mopeneko@users.noreply.github.com>
2020-04-11 15:05:11 +0900

    Make layout

---
+++ web/components/TopMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="enableDrawer" />
+      <v-toolbar-title>ぶいしゅき</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="isDrawerEnabled">
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      isDrawerEnabled: false
+    }
+  },
+
+  methods: {
+    enableDrawer() {
+      this.isDrawerEnabled = !this.isDrawerEnabled
+    }
+  }
+})
+</script>
+
